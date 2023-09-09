974f9eb218a4d276dc8f7b37e2e605f1a2f959f7
AdrianS100304 <jose13699631@gmail.com>
2023-09-08 18:39:05 -0600

    update

--- funciones/index.php
+++ funciones/index.php
@@ -1,17 +1,76 @@
 <?php
     #funciones sin parametros
-    function saludo(){
-        echo ("<h1>Hola</h1>");
-        echo ("<br> <br> Funciones sin parametros <br> <br> ");
+    function bannerBienvenida(){
+        echo ("<h1>Bienvenidos</h1>");
+        echo("A la pagina de APP Web <br>");
+        echo ("Iisis.co<br>");
     }
-    
-    saludo();
+
+    function bannerDespedida(){
+        echo("Hasta la vista <br>");
+        echo("Saludos de APP Web <br>");
+        echo("Iisis.co<br>");
+    }
+
+    bannerBienvenida();
+    echo("<br><br>");
+    bannerDespedida();
 
     #Funciones con parametros
-    function despedida($recibirParametro){
-        echo $recibirParametro;
-        echo ("<br> <br> Funciones sin parametros <br> <br> ");
+    $adios = "";
+    function despedida($adios){
+        echo $adios  . "mortal. <br>";
     }
+    despedida("See you later: ");
 
-    despedida("Hasta pronto ...");
+    $a = "hola hola hola ";
+    despedida($a);
+
+    echo("+++++++++++++++++++++++++++++++++++++++ <br>");
+    echo("+++++++++++++++++++++++++++++++++++++++ <br>");
+    despedida("$a");
+
+    $a = "  - UTT.";
+    echo("<br>"."+++++++++++++++++++++++++++++++++++++++ <br>");
+    echo("+++++++++++++++++++++++++++++++++++++++ <br>");
+    despedida("<br> Mi nombre es: Adrian." . $a);
+    
+
+    echo("+++++++++++++++++++++++++++++++++++++++ <br> <br> <br> <br>");
+
+    #Funciones con retorno
+    function retorno($retornar){
+        $retornar = $retornar .
+                                "Tercer tipo de funcion. <br>" . 
+                                "Y regresan un valor dado.";
+    
+    return $retornar;
+    }    
+    echo retorno("Tema: Funciones con retorno: " . "<br>");
+
+
+    echo(" <br> <br> +++++++++++++++++++++++++++++++++++++++ <br> <br> <br> <br>");
+    /*  De un valor dado de un producto, realizar una función
+        que se llame descuentoProducto, realice el calculo de
+        descuento de un 25%, Lo imprima en la página y que se
+        imprima una cadena que diga: Gracias por su compra,
+        su descuento fue de 20 pesos. 
+        Costo total es: $100 - $25 = $75.00 */
+/*
+     #Funciones con retorno
+     function descuentoProducto($descuento){
+        $descuento = "Gracias por su compra, <br>
+                    su descuento fue de " . $descuento = 100 * 0.25 . " pesos". 
+                        " <br> Costo total es:  $100 - $25 = $75.00 ";
+    return $descuento;
+    }    
+    echo descuentoProducto("");
+echo("<br>");*/
+
+    function descuentoProducto($precio){
+        $Descuento = $precio * 0.25;
+        $resultado ="Gracias por su compra, su descuento fue de: ". $Descuento. "<br>". "Costo total es: $ ". $precio . "-". $Descuento. "=". $precio*0.75. "<br>";
+        return $resultado;
+    }
+    echo descuentoProducto(100);
 ?>
